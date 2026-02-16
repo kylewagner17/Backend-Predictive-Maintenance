f60af5e916b81766615eb948debb0b9aa168ba34
kylewagner17 <60904904+kylewagner17@users.noreply.github.com>
2026-02-16 00:25:07 -0500

    test fix

--- app/ingest/plc.py
+++ app/ingest/plc.py
@@ -20,7 +20,7 @@ def poll_plc():
     result = client.read_holding_registers(
         address=START_REGISTER,
         count=REGISTER_COUNT,
-        device_id=1
+        device_id=0
     )
 
     if result.isError():
